4c4e70b84683e83468e064da5dc0e4dd5ae8031c
jujanree <jujanree@gmail.com>
2026-07-30 18:56:56 +0300

    adding `keys` and `values` functions to `map` submodule

--- src/map/main.ts
+++ src/map/main.ts
@@ -1,18 +1,23 @@
-import { Pair } from "../array/array.js"
-
 /**
  * Returns the pair of arrays of keys and values of the given `Map`
  */
-export const kv = (map: Map<any, any>): Pair<readonly any[]> => [
-	Array.from(map.keys()),
-	Array.from(map.values()),
-]
+export const kv = <K = any, V = any>(
+	map: Map<K, V>,
+): [readonly K[], readonly V[]] => [keys(map), values(map)]
 
 /**
  * Returns the new `Map` built from the key-value pairs passed
  */
-export const dekv = ([keys, values]: Pair<readonly any[]>): Map<any, any> => {
-	const map = new Map()
+export const dekv = <K = any, V = any>([keys, values]: [
+	readonly K[],
+	readonly V[],
+]): Map<any, any> => {
+	const map = new Map<K, V>()
 	for (let i = 0; i < keys.length; ++i) map.set(keys[i], values[i])
 	return map
 }
+
+export const keys = <K = any, V = any>(map: Map<K, V>) => Array.from(map.keys())
+
+export const values = <K = any, V = any>(map: Map<K, V>) =>
+	Array.from(map.values())
