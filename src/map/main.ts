b142eab43272e5f88be54e7f62da8e9cdf6434fa
jujanree <jujanree@gmail.com>
2026-08-09 21:32:29 +0300

    adding missing jsdoc for `map` module

--- src/map/main.ts
+++ src/map/main.ts
@@ -11,13 +11,19 @@ export const kv = <K = any, V = any>(
 export const dekv = <K = any, V = any>([keys, values]: [
 	readonly K[],
 	readonly V[],
-]): Map<any, any> => {
+]): Map<K, V> => {
 	const map = new Map<K, V>()
 	for (let i = 0; i < keys.length; ++i) map.set(keys[i], values[i])
 	return map
 }
 
+/**
+ * Returns an array of keys of a given `Map<K, V>`
+ */
 export const keys = <K = any, V = any>(map: Map<K, V>) => Array.from(map.keys())
 
+/**
+ * Returns an array of values of a given `Map<K, V>`
+ */
 export const values = <K = any, V = any>(map: Map<K, V>) =>
 	Array.from(map.values())
--- src/map/toObject.ts
+++ src/map/toObject.ts
@@ -1,8 +1,10 @@
 import { trivialCompose } from "../functional/functional.js"
-import { dekv } from "../object/object.js"
+import { dekv, ObjectKey } from "../object/object.js"
 import { kv } from "./main.js"
 
 /**
  * Converts a given `Map` into an `object`
  */
-export const toObject = trivialCompose(dekv, kv) as (x: Map<string, any>) => object
+export const toObject = trivialCompose(dekv, kv) as <T = any>(
+	x: Map<string, T>,
+) => Record<ObjectKey, T>
