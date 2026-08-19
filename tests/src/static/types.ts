d4d44e1a3adca6c9eae3cfb5cd1bed1520e5e778
jujanree <jujanree@gmail.com>
2026-08-19 13:29:50 +0300

    adding a new test case for the static `types.verifyConstructor` test

--- tests/src/static/types.ts
+++ tests/src/static/types.ts
@@ -5,28 +5,41 @@ import assert from "assert"
 const { verifyConstructor } = types
 
 suite("types", () => {
-	test("verifyConstructor", () => {
-		interface AObject {
-			readonly x: number
-			readonly m: number
-		}
+	suite("verifyConstructor", () => {
+		test("`this`-having function (constructor)", () => {
+			interface AObject {
+				readonly x: number
+				readonly m: number
+			}
 
-		function _A(m: number = 0) {
-			this.x = 10 + m
-			this.m = m
-		}
+			function _A(m: number = 0) {
+				this.x = 10 + m
+				this.m = m
+			}
 
-		const A = verifyConstructor<AObject>(_A)
-		const a1 = new A()
-		const a2 = new A(5)
+			const A = verifyConstructor<AObject>(_A)
+			const a1 = new A()
+			const a2 = new A(5)
 
-		assert.strictEqual(a1.m, 0)
-		assert.strictEqual(a1.x, 10)
-		assert.strictEqual(a2.m, 5)
-		assert.strictEqual(a2.x, 15)
+			assert.strictEqual(a1.m, 0)
+			assert.strictEqual(a1.x, 10)
+			assert.strictEqual(a2.m, 5)
+			assert.strictEqual(a2.x, 15)
 
-		// [static] Any of those should raise an in-editor error upon de-commenting
-		// a1.x = 11
-		// a2.m++
+			// [static] Any of those should raise an in-editor error upon de-commenting
+			// a1.x = 11
+			// a2.m++
+		})
+
+		test("non-`this`-having function (non-constructor)", () => {
+			interface CObject {}
+
+			let failed = false
+			try {
+				const _C = verifyConstructor<CObject>(() => {})
+				failed = true
+			} catch {}
+			assert(!failed)
+		})
 	})
 })
