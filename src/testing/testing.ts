fb1b32d4e6bbde872c1b071d0c63f39920a98d93
jujanree <jujanree@gmail.com>
2026-08-26 13:02:44 +0300

    adding jsdoc for the new `testing` module

--- src/testing/testing.ts
+++ src/testing/testing.ts
@@ -1,6 +1,9 @@
 import assert from "node:assert"
 import { isNumber } from "../types/types.js"
 
+/**
+ * Asserts that `callback` exits via throwing (an exception)
+ */
 export function assertThrows(callback: () => void) {
 	let failed = false
 
@@ -12,14 +15,26 @@ export function assertThrows(callback: () => void) {
 	assert(!failed)
 }
 
+/**
+ * Asserts that `a` is strictly greater than `b`
+ */
 export function assertGreaterThan<T = any>(a: T, b: T) {
 	assert(a > b)
 }
 
+/**
+ * Asserts that `a` is strictly less than `b`
+ */
 export function assertLessThan<T = any>(a: T, b: T) {
 	assert(a < b)
 }
 
+/**
+ * Asserts that `pred` is fulfilled for all the `x: T` of `iter`,
+ * with `i: number` parameter being the index of current `x` and
+ * `iter` itself being available inside the body of `pred` via the
+ * third argument.
+ */
 export function assertForEach<T, X extends Iterable<T>>(
 	iter: X,
 	pred: (x: T, i: number, iter: X) => boolean,
@@ -28,9 +43,28 @@ export function assertForEach<T, X extends Iterable<T>>(
 	for (const x of iter) assert(pred(x, i++, iter))
 }
 
+/**
+ * A guaranteedly finite `Iterable<number>` object with user-provided boundaries.
+ *
+ * @class
+ * @constructor
+ * @public
+ *
+ * @param {number} from The starting value for the range (inclusive). If `to` is not provided, `this.to` becomes the provided `from` and `this.from` becomes `0`.
+ * @param {number} to The endpoint of the range (exclusive). Provide this argument either if you need a non-`0` `this.from` value, or a non-`1` `this.step` value.
+ * @param {number} step The step of the range - the constant increment for obtaining new values. Default: `1`
+ */
 export class Range {
+	/**
+	 * The end boundary of the range (exclusive).
+	 */
 	declare readonly to: number
+
+	/**
+	 * The starting point of the range (inclusive).
+	 */
 	declare readonly from: number
+
 	declare private readonly limit: (i: number, lim: number) => boolean
 
 	private static less = (i: number, lim: number) => i < lim
@@ -47,6 +81,10 @@ export class Range {
 	constructor(
 		from: number,
 		to?: number,
+		/**
+		 * The iteration step - a constant value, by which the current range
+		 * value is incremented to produce the one immediately after.
+		 */
 		readonly step: number = 1,
 	) {
 		assert.notStrictEqual(step, 0)
