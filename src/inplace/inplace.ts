cf7f0a93a0b5855e603e0d2cbd8a4bfde84e29e0
jujanree <jujanree@gmail.com>
2026-08-26 13:50:57 +0300

    improving jsdoc for `inplace` module

--- src/inplace/inplace.ts
+++ src/inplace/inplace.ts
@@ -1,54 +1,64 @@
 /**
- * Sets the respective array values to be `mutation(array[i], i, array)`.
+ * Sets the `array[i]` to be `mutation(array[i], i, array)`
+ * for each `0 <= i < array.length`.
+ *
+ * Note: it changes the type of `array` to `Out[]`,
+ * potentially invalidating types of previous references.
+ *
  * @returns `array`
  */
-export function mutate<Type = any, OutType = any>(
-	array: Type[],
-	mutation: (x?: Type, i?: number, arr?: Type[]) => OutType
-): OutType[] {
+export function mutate<T = any, Out = any>(
+	array: T[],
+	mutation: (x?: T, i?: number, arr?: T[]) => Out,
+): Out[] {
 	let i = array.length
 	while (i--) array[i] = mutation(array[i], i, array) as any
-	return array as unknown as OutType[]
+	return array as unknown as Out[]
 }
 
 /**
- * Inserts `values` into `array` at `index`
+ * Inserts all of `values` into `array` starting at `index`.
+ *
  * @returns `array`
  */
-export const insert = <Type = any>(array: Type[], index: number, ...values: Type[]) => {
+export const insert = <T = any>(array: T[], index: number, values: T[]) => {
 	array.splice(index, 0, ...values)
 	return array
 }
 
 /**
- * Deletes `count` items from `array` at `index`
+ * Deletes `count` items from `array` starting at `index`.
+ *
  * @returns `array`
  */
-export const out = <Type = any>(array: Type[], index: number, count: number = 1) => {
+export const out = <T = any>(array: T[], index: number, count: number = 1) => {
 	array.splice(index, count)
 	return array
 }
 
 /**
- * Deletes last `count` items from `array`
+ * Deletes `count` items from the end of `array`.
+ *
  * @returns `array`
  */
-export const lastOut = <Type = any>(array: Type[], count = 1) => {
+export const lastOut = <T = any>(array: T[], count = 1) => {
 	array.length -= count
 	return array
 }
 
 /**
- * Deletes first `count` items from `array`
+ * Deletes `count` items from the beginning of `array`.
+ *
  * @returns `array`
  */
-export const firstOut = <Type = any>(array: Type[], count = 1) => out(array, 0, count)
+export const firstOut = <T = any>(array: T[], count = 1) => out(array, 0, count)
 
 /**
- * Changes places items at indexes `i` and `j` in the array
+ * Swaps places items at indexes `i` and `j` in the `array`.
+ *
  * @returns `array`
  */
-export const swap = <Type = any>(array: Type[], i: number, j: number) => {
+export const swap = <T = any>(array: T[], i: number, j: number) => {
 	const temp = array[i]
 	array[i] = array[j]
 	array[j] = temp
@@ -56,10 +66,11 @@ export const swap = <Type = any>(array: Type[], i: number, j: number) => {
 }
 
 /**
- * Replaces the value in `array` at `index` with `values`
- * @returns array
+ * Replaces the value in `array` with `values` starting at `index`.
+ *
+ * @returns `array`
  */
-export const replace = <Type = any>(array: Type[], index: number, ...values: Type[]) => {
+export const replace = <T = any>(array: T[], index: number, values: T[]) => {
 	array.splice(index, 1, ...values)
 	return array
 }
